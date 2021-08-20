938f4763e56c73abc1519a1ccdf901c4775b07df
AlanFerrari <alanferrari33@gmail.com>
2021-08-19 21:38:37 -0300

    first commit

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<?php 
+	#Comentário
+	#Data: 19/08/2021
+?>
+<head>
+	<title>Testando comentário</title>
+</head>
+<body>
+	<?php 
+		/*
+		Comentário de bloco
+		*/
+		echo "Segundo exemplo de comentário";
+		$data = "19 de agosto de 2021";
+	?>
+	<p>Este parágrafo está sendo interpretado pelo html</p>
+	Arquivo criado em php <?php echo "$data"; ?>
+</body>
+</html>
